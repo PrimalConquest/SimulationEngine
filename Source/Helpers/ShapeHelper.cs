4a21e66072237f1cecf699ab7806a97e23444a07
Ad04a <andrey.todorov04@gmail.com>
2026-03-31 22:37:35 +0300

    EmbedeedResource

---
+++ Source/Helpers/ShapeHelper.cs
@@ -0,0 +1,34 @@
+﻿using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.Reflection;
+using System.Text;
+
+namespace SimulationEngine.Source.Helpers
+{
+    public static class ShapeHelper
+    {
+        public static string[]? Parse(string id)
+        {
+            Assembly assembly = typeof(ShapeHelper).Assembly;
+            using (Stream stream = assembly.GetManifestResourceStream("SimulationEngine.Resources.Shapes.json"))
+            { 
+                using (StreamReader sr = new StreamReader(stream))
+                {
+                    string json = sr.ReadToEnd();
+
+                    List<ShapeInfo>? array = JsonConvert.DeserializeObject<List<ShapeInfo>>(json);
+
+                    if (array == null) return null;
+
+                    foreach (var shape in array)
+                    {
+                        if (shape.id == id) return shape.pattern;
+                    }
+                }
+            }
+            return null;
+        }
+
+    }
+}
---
+++ Source/Helpers/ShapeInfo.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace SimulationEngine.Source.Helpers
+{
+    internal class ShapeInfo
+    {
+        public string? id { get; set; }
+        public string[]? pattern { get; set; }
+    }
+}
